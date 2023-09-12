a69e1faa810e2d75a2452d684c45cf0686447b0c
Luciana Tokozima <lutokozima@gmail.com>
2023-09-12 19:40:11 -0300

    Conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO: conhecer e importar a classe Scanner
+        //obter pela classe Scanner os valores digitados no terminal
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+ 
+        //exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta !");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite nome do cliente !");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo !");
+        double saldo = scanner.nextDouble();
+
+        //exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente.toUpperCase()
+                            + ", obrigado por criar uma conta em nosso banco, sua agencia é "
+                            + agencia + ", conta "
+                            + numero + " e seu saldo "
+                            + saldo + " já está disponivel para saque."                            
+                            );
+
+    }
+
+    private static String upper(String nomeCliente) {
+        return null;
     }
 }
